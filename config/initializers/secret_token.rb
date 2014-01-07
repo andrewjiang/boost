5a53a6df0c631ac932b64276216f93e098c97e0d
Charlie Fang <charlie@zephyrcar.com>
2014-01-06 23:43:34 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Zephyr::Application.config.secret_key_base = 'b0e50683d47d10290f0d3ab03d90bdb53c3fdce57556cc7d85170d0c0670366804c07512e8b9dbdb8ba9908ed879e0791c98c87ce0ff8acd27de434ad23616fe'

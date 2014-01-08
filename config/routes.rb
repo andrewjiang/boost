630dc12373842d18d5a81131ce9fd580b914d06d
Charlie Fang <charlie@zephyrcar.com>
2014-01-08 00:42:15 -0800

    list and drive pages without forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Zephyr::Application.routes.draw do
 
   root 'home#index'
+  match '/list',  to: 'home#list',  via: 'get'
+  match '/drive', to: 'home#drive', via: 'get'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

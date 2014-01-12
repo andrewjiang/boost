3b284866512c56c036f7005c336f58a4350eb81b
Charlie Fang <charlie@zephyrcar.com>
2014-01-11 22:52:14 -0800

    settings required for sending emails in dev mode.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,6 +16,18 @@ Zephyr::Application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.default_url_options = { :host => 'localhost' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              '127.0.0.1',
+    port:                 1025,
+    domain:               'zephyrcar.com',
+    user_name:            'foo',
+    password:             'bar',
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 

7a872b37e6ed86573091ad67fb0ee85723206508
Charlie Fang <charlie@zephyrcar.com>
2014-01-13 21:44:39 -0800

    fix devise_helper

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -1,6 +1,7 @@
 module DeviseHelper
  def devise_error_messages!
-   return "" if resource.errors.empty? 
-   flash[:error] = resource.errors.full_messages[0]
+   return if resource.errors.empty?
+   flash[:alert] = resource.errors.full_messages[0]
+   return
  end
 end

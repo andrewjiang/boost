8b3a0f9bd8440ad6888a43ec0a31b5982ad7da58
Charlie Fang <charlie@zephyrcar.com>
2014-01-16 00:43:32 -0800

    default from value for email. also improve email template for driver app confirmation

--- app/mailers/driver_application_mailer.rb
+++ app/mailers/driver_application_mailer.rb
@@ -1,4 +1,5 @@
 class DriverApplicationMailer < ActionMailer::Base
+  default from: 'support@zephyrcar.com'
 
   def confirmation_email(driver_application)
     @driver_application = driver_application

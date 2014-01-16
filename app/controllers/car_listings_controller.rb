4c48cc0979ba194639d46c89e165e431ad76d943
andrewjiang <andrew.h.jiang@gmail.com>
2014-01-16 02:37:14 -0500

    Wording and minor stuff

--- app/controllers/car_listings_controller.rb
+++ app/controllers/car_listings_controller.rb
@@ -6,7 +6,7 @@ class CarListingsController < ApplicationController
   def create
     @car_listing = CarListing.new(car_listing_params)
     if @car_listing.save
-      flash[:"success alert alert-success ephemeral"] = "Thanks for listing your car! We will be contacting you shortly."
+      flash[:"success alert alert-success ephemeral"] = "Thanks for listing your car! A Zephyr car specialist will reach out if there is a match"
       redirect_to root_path
     else
       render 'new'

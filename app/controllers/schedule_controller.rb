c0e014cb3bc34963cc51acc94c411e788f980c3c
Charlie Fang <charlie@zephyrcar.com>
2014-01-20 20:57:47 -0800

    working car slot toggling

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -3,6 +3,7 @@ class ScheduleController < ApplicationController
   before_action :require_signed_in
 
   def show
+    @car_slots = [ {:id => 0, :name => "Monday", :status => "reserved"}, {:id => 1, :name => "Tuesday", :status => "cancelled"} ]
   end
 
 end

5d800b745f8f8a9da3b36209899300b5875563ac
Charlie Fang <charlie@zephyrcar.com>
2014-01-21 01:52:51 -0800

    sample seed data for development env. populate the db by calling "rake db:seed"

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+if Rails.env == 'development'
+  alice = User.create(
+    :email => "alice@example.com", 
+    :first_name => "Alice",
+    :last_name => "Summers",
+    :phone_number => "123-456-7891", 
+    :address => "123 Market St., San Francisco, CA", 
+    :password => "password")
+
+  bob = User.create(
+    :email => "bob@example.com", 
+    :first_name => "Bob",
+    :last_name => "Marley",
+    :phone_number => "912-1316-1232", 
+    :address => "631 Folsom St., San Francisco, CA", 
+    :password => "password")
+
+  d = DateTime.current.beginning_of_day
+  CarSlot.create(:start_time => d, :end_time => d.end_of_day(), :user_id => alice.id, :status => 'reserved', :fee => 50)
+  d = d.advance(:days => +1)
+  CarSlot.create(:start_time => d, :end_time => d.end_of_day(), :user_id => alice.id, :status => 'reserved', :fee => 50)
+  d = d.advance(:days => +1)
+  CarSlot.create(:start_time => d, :end_time => d.end_of_day(), :user_id => bob.id, :status => 'reserved', :fee => 50)
+  d = d.advance(:days => +1)
+  CarSlot.create(:start_time => d, :end_time => d.end_of_day(), :user_id => bob.id, :status => 'reserved', :fee => 50)
+  d = d.advance(:days => +1)
+  CarSlot.create(:start_time => d, :end_time => d.end_of_day(), :user_id => alice.id, :status => 'reserved', :fee => 50)
+  d = d.advance(:days => +1)
+  CarSlot.create(:start_time => d, :end_time => d.end_of_day(), :user_id => bob.id, :status => 'reserved', :fee => 50)
+  d = d.advance(:days => +1)
+  CarSlot.create(:start_time => d, :end_time => d.end_of_day(), :user_id => alice.id, :status => 'reserved', :fee => 50)
+
+end

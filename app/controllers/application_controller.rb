0d50989b5d211b1df8b5bc218d5cde15cef6cc17
Charlie Fang <charlie@zephyrcar.com>
2014-02-05 01:48:33 -0800

    fix redirect paths after sign in for users and admin users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def after_sign_in_path_for(user)
-    schedule_path
+  def after_sign_in_path_for(resource)
+    case resource
+    when User then schedule_path
+    when AdminUser then admin_root_path
+    end
   end
 end
